16ec843836f3f88c2ce28326c02b2df2008987c6
David Nazariego Ulacio <dnazariego@gmail.com>
2024-06-21 14:33:57 +0000

    tictactoe

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,90 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
 
-export default Home;
+function Square({ onClick, value }) {
+	return (
+	  <button className={`square ${value ? "square--active" : ""}`} onClick={onClick}>
+		{value}
+	  </button>
+	);
+  }
+  
+  function Board () {
+	const [squares, setSquares] = React.useState(Array(9).fill(null))
+	const [isX, setIsX] = React.useState(true);
+  
+	const handleClick = (i) => {
+	  if (calculateWinner(squares) || squares[i]) {
+		return
+	  }
+	  
+	  squares[i] = isX ? 'X' : 'O'
+	  setSquares(squares)
+	  setIsX(!isX)
+	}
+  
+	const winner = calculateWinner(squares)
+	let status
+	
+	if (winner) {
+	  status = `Winner: ${winner}`;
+	} else {
+	  status = 'Next player: ' + (isX ? 'X' : 'O');
+	}
+	
+	const handleRestart = () => {
+	  setIsX(true)
+	  setSquares(Array(9).fill(null))
+	}
+  
+	const renderSquare = (i) => {
+	  return <Square value={squares[i]} onClick={() => handleClick(i)} />
+	}
+	
+	return (
+	  <div className="board">
+		<div className="board-row">
+		  {renderSquare(0)}
+		  {renderSquare(1)}
+		  {renderSquare(2)}
+		</div>
+		<div className="board-row">
+		  {renderSquare(3)}
+		  {renderSquare(4)}
+		  {renderSquare(5)}
+		</div>
+		<div className="board-row">
+		  {renderSquare(6)}
+		  {renderSquare(7)}
+		  {renderSquare(8)}
+		</div>
+		<div className="status">{status}</div>
+		<button className="restart" onClick={handleRestart}>Restart Game!</button>
+	  </div>
+	)
+  }
+  
+ export default Board
+  
+  function calculateWinner(squares) {
+	const winningPatterns = [
+	  [0, 1, 2],
+	  [3, 4, 5],
+	  [6, 7, 8],
+	  [0, 3, 6],
+	  [1, 4, 7],
+	  [2, 5, 8],
+	  [0, 4, 8],
+	  [2, 4, 6],
+	]
+	
+	for (let i = 0; i < winningPatterns.length; i++) {
+	  const [a, b, c] = winningPatterns[i];
+	  if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+		return squares[a];
+	  }
+	}
+	return null;
+  }
